b9fb322c21535f12783992557d14643677bb5583
AbhinavSingh009 <abhinav.singh1009@gmail.com>
2017-11-24 23:49:06 -0500

    Changes to Controller and Login File

---
+++ controller/controller.php
@@ -0,0 +1,40 @@
+<?php
+  if(isset($_REQUEST['page_name']) && $_REQUEST['page_name'] == 'registration_data')
+  {
+      echo "asd";
+  }
+
+
+
+  //the main purpose of the controller is to send and recieve
+  //data need for all transactions betweeen UI and Mapper
+
+
+  //send login credentials
+
+
+
+  //check for client status to render page
+
+
+  //add
+
+
+
+  //delete
+
+
+
+
+  //modify
+
+
+
+  //holds wishlist from database
+
+
+
+
+  //temporary wishlist here
+
+?>
